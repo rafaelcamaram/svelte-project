fc76f642b54ca892f25edbd955759eae807bf780
Rafael Câmara <rafaelcamaram@gmail.com>
2019-09-17 18:02:47 -0300

    Initial files and structure

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Greeting from "./components/presentational/Greeting/Greeting.svelte";
+</script>
+
+<style lang="scss" type="text/scss" src="./App.scss">
+
+</style>
+
+<h1>Home Page</h1>
+<Greeting name="Rafael" />
+<p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id odio et
+  mauris luctus hendrerit. Sed nec dui arcu. Etiam mattis placerat nisi eu
+  tincidunt. Donec eu leo at erat eleifend lobortis. Suspendisse ullamcorper
+  nibh at metus finibus placerat non sed elit. Curabitur at elementum nunc.
+  Donec imperdiet eget dolor id dignissim.
+</p>
---
+++ src/components/presentational/Greeting/Greeting.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let name = "John";
+</script>
+
+<style lang="scss" type="text/scss" src="./Greeting.scss">
+
+</style>
+
+<h3>
+  Hello {name}
+  <span>!</span>
+</h3>
